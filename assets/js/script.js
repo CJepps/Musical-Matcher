ac64027fdefb2cd1caba927bd249f99c154637fa
CJepps <cjepps@hotmail.co.uk>
2021-11-21 11:55:23 +0000

    Fixed bug where game would not start when overlay was clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,41 @@
-if(document.readyState === 'loading') {                                                 /*makes sure that html loads before any JavaScript is run */
-    document.addEventListener('DOMContentLoaded', ready());
-    } else {
-        ready();
+class musicMatcherGame {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('moves');
+        this.starRating = document.getElementById('star-rating');
     }
+    startGame() {
+        this.cardToCheck = null;
+        this.totalMoves = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
 
-    
-
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.totalMoves++;
+            this.ticker.innerText = this.totalMoves;
+        } 
+    }
+    canFlipCard(card) {
+        return true;
+        //return (!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck)
+    }
+}    
 
 function ready() {                                                                   // All Javascript developed using Port Exe tutorial here: https://www.youtube.com/watch?v=3uuQ3g92oPQ&t=983s&ab_channel=PortEXE            
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'))
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new musicMatcherGame(120, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');                                       /*removes the visisble class when an overlay is clicked */ 
-                // add code here to start game when "click to play overlay is clicked " game.startgame
+            game.startGame();    
         });
     });
     cards.forEach(card => {
@@ -24,21 +45,9 @@ function ready() {                                                              
     });
 }
 
-class MusicMatcherGame {
-    constructor(totalTime, cards) {
-        this.cardsArray = cards;
-        this.totalTime = totalTime;
-        this.timeRemaining = totalTime;
-        this.timer = document.getElementById('time-remianing');
-        this.ticker = document.getElementById('moves');
-        this.starRating = document.getElementById('star-rating');
+if(document.readyState === 'loading') {                                                 /*makes sure that html loads before any JavaScript is run */
+    document.addEventListener('DOMContentLoaded', ready());
+    } else {
+        ready();
     }
-    startGame() {
-        this.cardToCheck = null;
-        this.totalClicks = 0;
-        this.timeRemaining = this.totalTime;
-        this.matchedCards = [];
-        this.busy = true;
-
-    }
-}
+    
